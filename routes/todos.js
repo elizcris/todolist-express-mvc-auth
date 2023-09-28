047688056a3e687abfcc6f005e48af45f8b00a6a
Elizabeth Cristina <lizzy.cristina@gmail.com>
2023-09-28 17:42:57 -0500

    Comment routes/todos.js file

--- routes/todos.js
+++ routes/todos.js
@@ -3,6 +3,7 @@ const router = express.Router()
 const todosController = require('../controllers/todos') 
 const { ensureAuth } = require('../middleware/auth')
 
+//ensureAuth before the controller ensures authentication, a function that checks to make sure user is logged in
 router.get('/', ensureAuth, todosController.getTodos)
 
 router.post('/createTodo', todosController.createTodo)
