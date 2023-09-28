8c3f28b7c69a4650fb3b478cd2396e8296e634ab
Elizabeth Cristina <lizzy.cristina@gmail.com>
2023-09-28 17:43:34 -0500

    Comment middleware/auth.js file

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,3 +1,4 @@
+//ensureAuth is a function checking to see if user is authenticated, if yes, move onto next thing, if no, redirect back to root
 module.exports = {
     ensureAuth: function (req, res, next) {
       if (req.isAuthenticated()) {
